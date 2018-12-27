117f5068c23fdc67194091675555a57c6b13f3b0
shearn89 <alexander.shearn@gmail.com>
2018-12-28 11:30:10 +1300

    Updating password hashing

--- site/role/manifests/base.pp
+++ site/role/manifests/base.pp
@@ -6,7 +6,7 @@ class role::base {
     groups         => [ 'wheel' ],
     home           => '/home/admin',
     managehome     => true,
-    password       => Sensitive(pw_hash('adminpassword'), 6),
+    password       => Sensitive(str2saltedsha512('adminpassword')),
     purge_ssh_keys => true,
   }
 
