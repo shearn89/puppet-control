b485316f0a946fe2cfe8fdff3edd666741508c04
shearn89 <alexander.shearn@gmail.com>
2018-12-28 14:18:33 +1300

    Updating password

--- site/role/manifests/base.pp
+++ site/role/manifests/base.pp
@@ -1,12 +1,14 @@
 class role::base {
 
+  $default_password = Sensitive(pw_hash('password', 6, fqdn_rand_string(16)))
+
   user { 'admin':
     ensure         => present,
     forcelocal     => true,
     groups         => [ 'wheel' ],
     home           => '/home/admin',
     managehome     => true,
-    password       => Sensitive(str2saltedsha512('adminpassword')),
+    password       => $default_password,
     purge_ssh_keys => true,
   }
 
